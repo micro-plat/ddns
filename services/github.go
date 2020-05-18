64ebb0af4433cd3fe94d7fd96af36b6153bfec89
zkfy <lib4net@163.com>
2020-05-18 16:53:11 +0800

    增加github ip检查

---
+++ services/github.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"github.com/micro-plat/hydra/component"
+	"github.com/micro-plat/hydra/context"
+)
+
+type GithubHandler struct {
+	container component.IContainer
+}
+
+func NewGithubHandler(container component.IContainer) (u *GithubHandler) {
+	return &GithubHandler{container: container}
+}
+
+//Handle 保存动态域名信息
+func (u *GithubHandler) Handle(ctx *context.Context) (r interface{}) {
+	ctx.Log.Info("--------------保存动态域名信息---------------")
+
+	ctx.Log.Info("1.获取参数")
+	domians, err := GetGithubDomains()
+	if err != nil {
+		return err
+	}
+
+	for _, v := range domians {
+		ctx.Log.Infof("2. 获取分布式锁,v%+v", v)
+		lk := ctx.NewDLock(v.Domain)
+		if err := lk.Lock(); err != nil {
+			return err
+		}
+		defer lk.Unlock()
+
+		ctx.Log.Info("3. 检查并创建解析信息")
+		if err := checkAndCreate(v, u.container.GetRegistry()); err != nil {
+			return err
+		}
+	}
+
+	return "success"
+}
