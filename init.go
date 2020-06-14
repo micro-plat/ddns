38a7bc3cee42c294d8b440a45ac9ebc6fe7007d1
zkfy <lib4net@163.com>
2020-06-14 11:02:59 +0800

    增加定时获取github.com

--- init.go
+++ init.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/micro-plat/ddns/services"
+	"github.com/micro-plat/hydra"
 )
 
 //init 检查应用程序配置文件，并根据配置初始化服务
@@ -9,5 +10,10 @@ func init() {
 
 	app.Micro("/ddns/request", services.NewDdnsHandler())
 	app.Micro("/github/ip/check", services.NewGithubHandler())
-	app.CRON("/github/ip/check", services.NewGithubHandler())
+	app.CRON("/github/ip/check", services.NewGithubHandler(), "@every 50s")
+
+	hydra.Conf.OnReady(func() {
+		hydra.CRON.Add("@now", "/github/ip/check")
+	})
+
 }
