6ae80e41a768147acc7b09a1f3bf0beaf4f7b822
zkfy <lib4net@163.com>
2020-12-15 09:58:30 +0800

    更新服务

--- dns/query/hosts_darwin.go
+++ dns/query/hosts_darwin.go
@@ -1,7 +1,6 @@
 package query
 
 const (
-	Resolve_File = "/etc/resolv.conf"
-	HOST_FILE    = "/etc/host*"
-	NAME_FILE    = "/etc/names.conf"
+	HOST_FILE = "/etc/host*"
+	NAME_FILE = "/etc/names.conf"
 )
--- dns/query/hosts_windows.go
+++ dns/query/hosts_windows.go
@@ -1,7 +1,6 @@
 package query
 
 const (
-	Resolve_File = `C:\Windows\System32\drivers\etc\resolv*`
-	HOST_FILE    = `C:\Windows\System32\drivers\etc\host*`
-	NAME_FILE    = `C:\Windows\System32\drivers\etc\names.conf`
+	HOST_FILE = `C:\Windows\System32\drivers\etc\host*`
+	NAME_FILE = `C:\Windows\System32\drivers\etc\names.conf`
 )
