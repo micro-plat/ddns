9b59274bf59fe5be9968bf7bc55d5dadd51d3f67
hj <546921722@qq.com>
2020-12-16 14:47:04 +0800

    修改静态html

--- init.go
+++ init.go
@@ -5,8 +5,8 @@ import (
 	"github.com/micro-plat/ddns/dns"
 	"github.com/micro-plat/ddns/services"
 	"github.com/micro-plat/hydra"
-	"github.com/micro-plat/hydra/conf/server/api"
 	"github.com/micro-plat/hydra/conf/server/cron"
+	"github.com/micro-plat/hydra/conf/server/static"
 )
 
 //init 检查应用程序配置文件，并根据配置初始化服务
@@ -17,7 +17,7 @@ func init() {
 	dns.App.Micro("/github/ip/*", services.NewGithubHandler())
 	dns.App.CRON("/github/ip/*", services.NewGithubHandler(), "@midnight")
 
-	hydra.Conf.API(":8081", api.WithDNS("www.ddns.com"))
+	hydra.Conf.API(":8081").Static(static.WithArchive("./assert/assert.so"), static.WithRoot("./assert"), static.WithEnable())
 	hydra.Conf.CRON(cron.WithMasterSlave())
 	hydra.CRON.Add("@now", "/github/ip/request")
 
