3aff60cbba6162b6a0043a5a2393a135b7fca4c0
zkfy <lib4net@163.com>
2020-12-16 17:08:07 +0800

    添加rtt排序

---
+++ names/rtt.go
@@ -0,0 +1,27 @@
+package names
+
+import "sync/atomic"
+
+type NameRTT struct {
+	name       string
+	maxRequest int64
+	avgRTT     int64
+}
+
+//Update 更新请求时间
+func (n *NameRTT) Update(t int64) {
+	max := atomic.AddInt64(&n.maxRequest, 1)
+	n.avgRTT = (n.avgRTT*(max-1) + t) / max
+}
+
+type RTTS []*NameRTT
+
+func (s RTTS) Len() int { return len(s) }
+
+func (s RTTS) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+func (s RTTS) Less(i, j int) bool {
+	left := s[i].avgRTT
+	right := s[j].avgRTT
+	return left < right
+}
