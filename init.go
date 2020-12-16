bbe9dcacf9faa7d0a632a6987410641ed4f20f6f
hj <546921722@qq.com>
2020-12-16 19:03:05 +0800

    静态文件处理

--- init.go
+++ init.go
@@ -11,13 +11,15 @@ import (
 
 //init 检查应用程序配置文件，并根据配置初始化服务
 func init() {
-	hydra.Conf.Custom(dns.DDNS, conf.New(conf.WithTimeout(10, 10), conf.WithTrace())).
-		Sub(conf.TypeNodeName, conf.NewNames("114.114.114.114", "8.8.8.8"))
+	hydra.G.RegistryAddr = "zk://192.168.0.101"
+
+	hydra.Conf.Custom(dns.DDNS, conf.New(conf.WithTimeout(10, 10))).
+		Sub(conf.TypeNodeName, conf.NewNames("192.168.5.115", "114.114.114.114"))
 	dns.App.Micro("/ddns", services.NewDdnsHandler())
-	// dns.App.Micro("/github/ip/*", services.NewGithubHandler())
+	dns.App.Micro("/github/ip/*", services.NewGithubHandler())
 	// dns.App.CRON("/github/ip/*", services.NewGithubHandler(), "@midnight")
 
-	hydra.Conf.API(":8081").Static(static.WithArchive("./assert/assert.so"), static.WithRoot("./assert"), static.WithEnable())
+	hydra.Conf.API(":8081").Static(static.WithArchive(archive))
 	hydra.Conf.CRON(cron.WithMasterSlave())
 	// hydra.CRON.Add("@now", "/github/ip/request")
 
