22afb99f8380808ecb1bc4d07c22e1fa47ff8b82
zkfy <lib4net@163.com>
2020-12-18 20:29:14 +0800

    更改服务初始化代码

--- dns/web.go
+++ dns/web.go
@@ -1,15 +1,26 @@
 package dns
 
 import (
+	"github.com/micro-plat/ddns/conf"
+	"github.com/micro-plat/ddns/services"
 	"github.com/micro-plat/hydra"
 	"github.com/micro-plat/hydra/conf/server/api"
-	"github.com/micro-plat/hydra/conf/server/header"
+	"github.com/micro-plat/hydra/conf/server/cron"
+	"github.com/micro-plat/hydra/conf/server/static"
 )
 
 func init() {
-	hydra.Conf.API(":9090", api.WithTimeout(300, 300)).
-		Header(header.WithCrossDomain())
 
-	//_dns_bootstrap_js
+	//初始化服务器配置
+	hydra.Conf.Custom(DDNS, conf.New(conf.WithTimeout(10, 10))).
+		Sub(conf.TypeNodeName, conf.NewNames("8.8.8.8"))
+	hydra.Conf.Web(":80", api.WithDNS("ddns.com")).Static(static.WithArchive(services.Archive))
+	hydra.Conf.API(":8081", api.WithDNS("ddns.com"))
+	hydra.Conf.CRON(cron.WithMasterSlave())
+
+	//注册服务
+	App.Micro("/ddns/*", services.NewDdnsHandler())
+	App.Micro("/github/ip/*", services.NewGithubHandler())
+	App.CRON("/github/ip/*", services.NewGithubHandler(), "@midnight", "@now")
 
 }
--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"time"
-
 	"github.com/micro-plat/ddns/dns"
 	"github.com/micro-plat/hydra"
 	"github.com/micro-plat/hydra/global/compatible"
+	"github.com/micro-plat/lib4go/logger"
 )
 
 func main() {
 	if err := compatible.CheckPrivileges(); err != nil {
+		defer logger.Close()
 		hydra.G.Log().Error(err)
-		time.Sleep(time.Second)
 		return
 	}
 	dns.App.Start()
