07c898e8ff2718fb34b7e2ac9082850427b9024e
zkfy <lib4net@163.com>
2020-12-22 14:12:48 +0800

    更新registry

--- dns/app.go
+++ dns/app.go
@@ -17,8 +17,8 @@ import (
 
 //App dns应用程序
 var App = hydra.NewApp(
-	hydra.WithPlatName("ddns", "dns平台"),
-	hydra.WithSystemName("ddns", "dns系统"),
+	hydra.WithPlatName("ddns", "DDNS"),
+	hydra.WithSystemName("ddns", "域名解析"),
 	hydra.WithUsage("DNS服务"),
 	hydra.WithServerTypes(DDNS, c.CRON, http.Web),
 	hydra.WithClusterName("dns-1.2"),
