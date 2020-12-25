0a169a128026bb981972d04659b429012eeb7132
zkfy <lib4net@163.com>
2020-12-25 10:36:08 +0800

    添加轮询算法

--- names/sort.go
+++ names/sort.go
@@ -10,18 +10,22 @@ import (
 )
 
 type Sorter struct {
-	sorted  []string
-	rtts    cmap.ConcurrentMap
-	lock    sync.RWMutex
-	closeCh chan struct{}
-	index   int32
+	fastList []string
+	slowList []string
+	rtts     cmap.ConcurrentMap
+	lock     sync.RWMutex
+	closeCh  chan struct{}
+	maxRTT   int64
+	index    int32
 }
 
 func newSorter() *Sorter {
 	r := &Sorter{
-		sorted:  make([]string, 0, 1),
-		rtts:    cmap.New(3),
-		closeCh: make(chan struct{}),
+		fastList: make([]string, 0, 1),
+		slowList: make([]string, 0, 1),
+		rtts:     cmap.New(3),
+		maxRTT:   int64(time.Millisecond * 30),
+		closeCh:  make(chan struct{}),
 	}
 	go r.loopUpdate()
 	return r
@@ -31,7 +35,7 @@ func newSorter() *Sorter {
 func (s *Sorter) Sort(names ...string) []string {
 
 	//初始前不进行排序
-	if len(s.sorted) == 0 {
+	if len(s.fastList) == 0 {
 		return names
 	}
 
@@ -43,22 +47,33 @@ func (s *Sorter) Sort(names ...string) []string {
 
 	atomic.CompareAndSwapInt32(&s.index, 2<<20, 1)
 	index := atomic.AddInt32(&s.index, 1)
+
 	//锁定与解锁
 	s.lock.RLock()
 	defer s.lock.RUnlock()
 
 	//以排序好的服务器控制顺序
 	sorted := make([]string, 0, len(names))
-	current := s.sorted[int(index%int32(len(s.sorted)))]
+	cindex := int(index % int32(len(s.fastList)))
+	current := s.fastList[cindex]
 	sorted = append(sorted, current)
-	for _, v := range s.sorted {
-		if v != current {
+
+	//加入较快的DNS
+	for i, v := range s.fastList {
+		if i != cindex {
 			if _, ok := dist[v]; ok {
 				sorted = append(sorted, v)
 				dist[v] = true
 			}
 		}
+	}
 
+	//加入较慢的DNS
+	for _, v := range s.slowList {
+		if _, ok := dist[v]; ok {
+			sorted = append(sorted, v)
+			dist[v] = true
+		}
 	}
 
 	//未列入排序列表的，直接加入返回列表
@@ -86,23 +101,30 @@ func (s *Sorter) loopUpdate() {
 		select {
 		case <-s.closeCh:
 			return
-		case <-time.After(time.Minute * 5): //每5分钟更新一次列表
+		case <-time.After(time.Minute * 2): //每2分钟更新一次列表
 			s.updateList()
 		}
 	}
-
 }
 
 func (s *Sorter) updateList() {
 	items := s.rtts.Items()
-	list := make(RTTS, 0, len(items))
+	fastList := make(RTTS, 0, len(items))
+	slowList := make(RTTS, 0, 1)
 	for _, v := range items {
-		list = append(list, v.(*NameRTT))
+		rtt := v.(*NameRTT)
+		if rtt.avgRTT < s.maxRTT {
+			fastList = append(fastList, rtt)
+		} else {
+			slowList = append(slowList, rtt)
+		}
 	}
-	sort.Sort(list)
+	sort.Sort(fastList)
+	sort.Sort(slowList)
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	s.sorted = list.ToList()
+	s.fastList = fastList.ToList()
+	s.slowList = slowList.ToList()
 }
 
 //Close 关闭服务
